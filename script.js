96b0f0de3c01fc82c554bc5abc4d2cab7f2d61e4
Julia-itcourse <66180630+Julia-itcourse@users.noreply.github.com>
2020-08-12 00:01:31 +0200

    добавляет задачу 1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//? task 1
+//? task 1 Color Change
 
 const colors = [
   "#FFFFFF",
@@ -14,10 +14,35 @@ const refs = {
   startBtn: document.querySelector('[data-action="start"]'),
   stopBtn: document.querySelector('[data-action="stop"]'),
 }
+let timerId = null
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
+
+const colorChange = () => {
+  const randomBgColor = colors[randomIntegerFromInterval(0, colors.length)]
+  refs.body.style.backgroundColor = `${randomBgColor}`
+}
+
+const stopColorChange = () => {
+  if (timerId !== null) {
+    clearInterval(timerId)
+  }
+}
+
+refs.startBtn.addEventListener("click", () => {
+  console.log("startBtn")
+  timerId = setInterval(colorChange, 1000)
+  refs.startBtn.disabled = true
+  refs.startBtn.style.backgroundColor = "gray"
+})
+
+refs.stopBtn.addEventListener("click", () => {
+  stopColorChange()
+  refs.startBtn.disabled = false
+  refs.startBtn.style.backgroundColor = "#8add7f"
+})
 
 //? task 2
 
