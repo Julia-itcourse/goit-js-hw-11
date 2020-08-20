473c558dfc56a0f34be4ff9e0b4036cba12eb35e
Julia-itcourse <66180630+Julia-itcourse@users.noreply.github.com>
2020-08-20 11:35:38 +0200

    update timer task

--- script.js
+++ script.js
@@ -112,43 +112,46 @@
 
 //?  _____________________Timer_________________________________________________
 
-// const refs = {
-//   sec: document.querySelector('[data-value="secs"]'),
-//   min: document.querySelector('[data-value="mins"]'),
-//   hour: document.querySelector('[data-value="hours"]'),
-//   day: document.querySelector('[data-value="days"]'),
-// }
+const refs = {
+  sec: document.querySelector('[data-value="secs"]'),
+  min: document.querySelector('[data-value="mins"]'),
+  hour: document.querySelector('[data-value="hours"]'),
+  day: document.querySelector('[data-value="days"]'),
+}
 
-// const timer = {
-//   start() {
-//     const targetTime = new Date("Sept 01, 2020")
+const timer = {
+  start() {
+    const targetTime = new Date("Sept 01, 2020")
 
-//     setInterval(() => {
-//       const currentTime = Date.now()
+    setInterval(() => {
+      const currentTime = Date.now()
 
-//       const delta = targetTime - currentTime
+      const delta = targetTime - currentTime
 
-//       updateTime(delta)
-//     }, 1000)
-//   },
-// }
+      updateTime(delta)
+    }, 1000)
+  },
+}
 
-// function updateTime(time) {
-//   const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)))
-//   const hours = pad(
-//     Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-//   )
-//   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)))
-//   const secs = pad(Math.floor((time % (1000 * 60)) / 1000))
+const updateTime = (time) => {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)))
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  )
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)))
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000))
+  updateValues(secs, mins, hours, days)
+}
 
-//   refs.sec.textContent = secs
-//   refs.min.textContent = mins
-//   refs.hour.textContent = hours
-//   refs.day.textContent = days
-// }
+const updateValues = (secs, mins, hours, days) => {
+  refs.sec.textContent = secs
+  refs.min.textContent = mins
+  refs.hour.textContent = hours
+  refs.day.textContent = days
+}
 
-// function pad(value) {
-//   return String(value).padStart(2, "0")
-// }
+const pad = (value) => {
+  return String(value).padStart(2, "0")
+}
 
-// timer.start()
+timer.start()
